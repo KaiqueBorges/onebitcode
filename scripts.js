97420ba579eb280008ffaeb5e100323c4b1b04e3
KaiqueBorges <kaiqueborges_255@hotmail.com>
2022-06-23 20:38:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,31 @@
 function calculateTip(event) {
-  event.preventDefault()
-  let bill = document.getElementById('bill').value
-  let serviceQuality = document.getElementById('serviceQuality').value
-  let numOfpeople = document.getElementById('people').value
-
-  if ((bill == '') | (serviceQuality == 0)) {
-    alert('Por favor preencha os valores')
-    return
+event.preventDefault();
+console.log(event);
+let bill = document.getElementById('bill').value;
+let serviceQual = document.getElementById('serviceQual').value;
+let numOfPeople = document.getElementById('people').value;
+  if(bill == '' | serviceQual == 0){
+    alert("Por favor, preencha os valores")
+    return;
   }
 
-  if ((numOfpeople == '') | (numOfpeople <= 1)) {
-    numOfpeople = 1
-    document.getElementById('each').style.display = 'none'
+  if(numOfPeople == "" | numOfPeople <=1) {
+    numOfPeople = 1;
+    document.getElementById('each').style.display = "none"
   } else {
-    document.getElementById('each').style.display = 'block'
+    document.getElementById('each').style.display = "block"
   }
 
-  let total = (bill * serviceQuality) / numOfpeople
-  total = total.toFixed(2)
-  document.getElementById('tip').innerHTML = total
-  document.getElementById('totalTip').style.display = 'block'
+  let total = (bill * serviceQual) / numOfPeople;
+  total = total.toFixed(2);
+  document.getElementById('tip').innerHTML = total;
+  document.getElementById('totalTip').style.display = "block";
+
+
 }
 
-document.getElementById('totalTip').style.display = 'none'
-document.getElementById('each').style.display = 'none'
+document.getElementById('totalTip').style.display = "none";
+document.getElementById('each').style.display = "none";
+
 
 document.getElementById('tipsForm').addEventListener('submit', calculateTip)
